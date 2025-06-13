99b88cf6599ebadf568243a4c4e75cac0b90fb8d
Lucas Barrena <lucasbarrena98@gmail.com>
2025-06-13 16:24:10 -0300

    v3 (multiple tables)

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const CRC = new Uint32Array(256)
+const CRC = Array.from({ length: 8 }, () => new Uint32Array(256))
 
 for (let i = 0; i < 256; i++) {
   let x = i
@@ -7,16 +7,40 @@ for (let i = 0; i < 256; i++) {
     x = (x & 1) ? (0xEDB88320 ^ (x >>> 1)) : (x >>> 1)
   }
 
-  CRC[i] = x >>> 0
+  CRC[0][i] = x >>> 0
+}
+
+for (let z = 1; z < 8; z++) {
+  for (let i = 0; i < 256; i++) {
+    CRC[z][i] = (CRC[z - 1][i] >>> 8) ^ CRC[0][CRC[z - 1][i] & 0xFF]
+  }
 }
 
 module.exports = function crc32 (buf) {
   if (typeof buf === 'string') buf = Buffer.from(buf)
 
   let crc = ~0
+  let i = 0
 
-  for (let i = 0; i < buf.length; i++) {
-    crc = CRC[(crc ^ buf[i]) & 0xFF] ^ (crc >>> 8)
+  for (; i + 8 <= buf.length; i += 8) {
+    crc ^= buf[i] |
+          (buf[i + 1] << 8) |
+          (buf[i + 2] << 16) |
+          (buf[i + 3] << 24)
+
+    crc =
+      CRC[7][crc & 0xFF] ^
+      CRC[6][(crc >>> 8) & 0xFF] ^
+      CRC[5][(crc >>> 16) & 0xFF] ^
+      CRC[4][crc >>> 24] ^
+      CRC[3][buf[i + 4]] ^
+      CRC[2][buf[i + 5]] ^
+      CRC[1][buf[i + 6]] ^
+      CRC[0][buf[i + 7]]
+  }
+
+  for (; i < buf.length; i++) {
+    crc = CRC[0][(crc ^ buf[i]) & 0xFF] ^ (crc >>> 8)
   }
 
   return ~crc >>> 0
